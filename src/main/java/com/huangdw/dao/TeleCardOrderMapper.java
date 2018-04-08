574ad95cbcb7581b055420ec9af8e4c4b7440626
huangdw <hdw714608565@163.com>
2018-04-08 17:26:39 +0800

    使用mybatis逆向工程生成代码

--- src/main/java/com/huangdw/dao/TeleCardOrderMapper.java
+++ src/main/java/com/huangdw/dao/TeleCardOrderMapper.java
@@ -1,16 +1,17 @@
 package com.huangdw.dao;
 
 import com.huangdw.entity.TeleCardOrder;
-import java.util.List;
 
 public interface TeleCardOrderMapper {
     int deleteByPrimaryKey(Long id);
 
     int insert(TeleCardOrder record);
 
+    int insertSelective(TeleCardOrder record);
+
     TeleCardOrder selectByPrimaryKey(Long id);
 
-    List<TeleCardOrder> selectAll();
+    int updateByPrimaryKeySelective(TeleCardOrder record);
 
     int updateByPrimaryKey(TeleCardOrder record);
 }
